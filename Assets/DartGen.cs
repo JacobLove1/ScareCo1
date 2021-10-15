6895c5744dffd608ef4102b6201f95e31050de1f
Jacob Love <jlove29@uic.edu>
2021-10-15 14:13:46 -0500

    'starter'

---
+++ Assets/DartGen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DartGen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform Spawnpoint;
+    public GameObject Prefab;
+    public bool pressed;
+
+    void Start()
+    {
+        pressed = false;
+        InvokeRepeating("PressState", 2f, 1f);
+    }
+    void PressState() {
+        pressed = false;
+    }
+    void OnTriggerEnter()
+    {
+        if (pressed == false)
+        {
+            GameObject instance = GameObject.Instantiate(Prefab) ;
+            pressed = true;
+        }
+
+    }
+}
